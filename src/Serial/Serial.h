7995a8c0a061234fa944c69d4d3560c54122de3c
Даниил Петров <daniilpetrov@MacBook-Pro-Daniil.local>
2020-10-19 23:40:37 +0400

    [+] - добавлен новый класс отвечающий за обработку сообщений с сериального порта

--- src/Serial/Serial.h
+++ src/Serial/Serial.h
@@ -1,39 +1,47 @@
 #pragma once
 
+#define _CRT_SECURE_NO_WARNINGS
+
+
 #include <tchar.h>
-#include <windows.h>
 #include <stdio.h>
 #include <string>
 #include <iostream>
+//#include "../Storage/Storage.h"
+#include "../general_types.h"
+
 
 bool OpenPort(HANDLE* hName, char aPort[], DWORD baudrate);
 void ClosePort(HANDLE* hName);
 bool WriteinPort(HANDLE* hName, char mes[], size_t numbytes);
 int ReadPort(HANDLE* hName, char mes[], size_t numbytes);
 
+#define START_MES	0x0a
+#define END_MES		0x0d
+
 class SerialPort
 {
 public:
-	SerialPort(std::string port_name, long baudrate);
+	SerialPort(std::string &port_name, long baudrate);
 	~SerialPort();
-	void process();
-
-private:
-	inline void OpenPort(std::string port_name, long buadrate);
-	inline void ClosePort();
-	inline bool PortisValid() { return (port != INVALID_HANDLE_VALUE); }
-
+	//void process();
 	/*
-		функции работы с портом спр€таны внутри класса, так как будут вызыватьс€ внутри process()
+	функции работы с портом спр€таны внутри класса, так как будут вызыватьс€ внутри process()
 	*/
 	size_t WriteToPort(char *buf, size_t numbutes);
 	size_t ReadAllFromPort(char* buf, size_t numbytes);
-	size_t ReadMesFromPort(char* buf, size_t numbytes);
-
-
-
-
+	size_t ReadMesFromPort(char* buf);
+	inline HANDLE *getportptr() { return &port; }
+private:
+	inline void OpenPort(std::string &port_name, long buadrate);
+	inline void ClosePort();
+	inline bool PortisValid() { return (port != INVALID_HANDLE_VALUE); }
 
 private:
 	HANDLE port;
+
+	/*
+		общее хранилище дл€ всех экземпл€ров класса
+	*/
+	//Storage storage;
 };
--- src/Storage/Storage.cpp
+++ src/Storage/Storage.cpp
@@ -3,12 +3,61 @@
 
 
 
-// buffer_t *Storage::GetMesByPortId(HANDLE* port)
-// {
-// 	auto itr = find_if(storage.begin(), storage.end(), [=](buffer_t *mes)
-// 		{
-// 			return (mes->src == port);
-// 		});
-// 
-// 	return (itr != storage.end() ? storage[itr] : nullptr);
-// }
+Storage::Storage() : 
+	users(0)
+{
+	storage.reserve(50);
+}
+
+Storage::~Storage()
+{
+	;
+}
+
+void Storage::GetMesByPortId(HANDLE* port, _buffer_t &mes)
+{
+	auto itr = find_if(storage.begin(), storage.end(), [=](_buffer_t mes)
+		{
+			return (mes.src != port);
+		});
+
+
+	if ((itr != storage.end()))
+		mes = *itr;
+	else
+		mes.size = 0;
+}
+
+void Storage::PushMes(_buffer_t &mes)
+{
+	if(mes.size > 0)
+		this->storage.push_back(mes);
+}
+
+//template <typename T>
+size_t RingStorage :: getemptyidx()
+{
+	return (current_idx++) % STORAGE_SIZE;
+}
+
+void MesStorage :: push(HANDLE *port, _buffer_t &mes)
+{
+	storage[getemptyidx()].data = mes;
+	storage[getemptyidx()].data.src = port;
+	storage[getemptyidx()].status = _FULL;
+}
+
+void MesStorage::pop(HANDLE *port, _buffer_t &mes)
+{
+	mes.size = 0;
+
+	for (size_t idx = 0; idx < STORAGE_SIZE; idx++)
+	{
+		if (storage[idx].status == _FULL && storage[idx].data.src == port)
+		{
+			mes = storage[idx].data;
+		}
+	}
+
+	return;
+}
